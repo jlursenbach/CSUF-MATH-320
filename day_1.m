553151316c4972c9103b46ef839612687a43271e
Jacob Ursenbach <jacob.ursenbach@gmail.com>
2022-08-22 12:44:50 -0700

    Update and rename day_1 to day_1.m

---
+++ day_1.m
@@ -0,0 +1,32 @@
+%% Section 1.1 Starting MatLab
+% command window
+% editor
+% help
+% COMMAND HISTORY
+% CURRENT DIRECTORY
+% workspace (variable names and sizes)
+
+7 + 2; % execute but supress display
+
+%% Section 1.2 Working in Command Window
+% case sensitive
+% hit return to excecute
+% semi colon executes but supresses
+% comma excecutes several commands than displays
+% comment (%)
+% clc: clear command window
+
+%% Section 1.3 Operators and Precedence
+
+% Operators:
+
+% normal operation (+ - * /)
+% exponential(exp)
+% power: 5^2
+% logarithm log = ln and log10 = log with base 10
+
+% Precedence:
+
+% (1) parentheses (inner most to outer)
+% (2) Exponential/Transpose/Negate(~)
+%
