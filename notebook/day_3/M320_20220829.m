78641aff399b4338342afab37b1771da6bc2e791
Jacob Ursenbach <jacob.ursenbach@gmail.com>
2022-08-29 11:44:58 -0700

    Update M320_20220829.m

--- notebook/day_3/M320_20220829.m
+++ notebook/day_3/M320_20220829.m
@@ -1,1 +1,11 @@
 
+%% Section 1.7 Useful Commands for Managing Variables
+clear % clear all variables in the workspace
+% NOTE: it's best to start with clear to avoid ambiguity in variables. 
+clear b c; % just clears variables b and c
+
+clc % clear command window
+
+who % what are the current variables in the workspace
+whos % current variables in the workspace and their sizes
+
