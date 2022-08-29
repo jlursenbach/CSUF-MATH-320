160886b2b435048adfd4097642a9c85b732fb000
Jacob Ursenbach <jacob.ursenbach@gmail.com>
2022-08-29 11:58:57 -0700

    Update M320_20220829.m

--- notebook/day_3/M320_20220829.m
+++ notebook/day_3/M320_20220829.m
@@ -7,5 +7,24 @@ clear b c; % just clears variables b and c
 clc % clear command window
 
 who % what are the current variables in the workspace
-whos % current variables in the workspace and their sizes
+whos % current variables in the workspace and their sizes etc
+% whos columns: size / bytes / class (type) / attributes
+
+length(a) % defines the len of vector/matrix a
+size(a) % number of rows and columns in a
+
+% lets create a vector:
+a = 1:2:10 
+
+% a is now == {1,3,5,7,9)
+length(a) % 5 
+size(a) % 1  5 
+
+isempty(a) % checks if a variable is empty (Yes = 1 No = 2)
+
+%% section 1.8 Script Files
+% script file can be executed without passing in any variables.  (stand alone)
+% script executes as is written
+% each statement must be chronologicy defined and in correct syntax
+
 
